e533fb76716c5bef04e9819af8a6dd14e7e4a537
Oscar Solis <oscar@blacklabs.mx>
2018-10-18 13:45:36 -0600

    Se agrega la ruta hacia la tabla de "Carreras".

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         <a class="btn btn-primary" href="routes/archivo.php" role="button">Archivo</a>
         <a class="btn btn-primary" href="routes/asistencias.php" role="button">Asistencias</a>
         <a class="btn btn-primary" href="routes/becas.php" role="button">Becas</a>
+        <a class="btn btn-primary" href="routes/alumnos.php" role="button">Carreras</a>
         <a class="btn btn-primary" href="routes/ciclos.php" role="button">Ciclos escolares</a>
         <a class="btn btn-primary" href="routes/estados.php" role="button">Estados</a>
         <a class="btn btn-primary" href="routes/usuarios.php" role="button">Usuarios</a>
