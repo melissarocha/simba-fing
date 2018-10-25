571427317aa7db6001a02dac7b6fbc7c1a1a9785
Melissa Rocha <emelissaro97@gmail.com>
2018-10-24 21:10:20 -0600

    Se implementa header y footer dentro de la aplicacion

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-    <div class="row">
-        <h2>Simba</h2>
-        <h4>Sistema de Becas - Facultad de ingeniería</h4>
+<?php
+include ('views/header.php');
+?>
+
+    <div class="jumbotron">
+        <h1 class="display-4">Hola, bienvenido a SIMBA!</h1>
+        <p class="lead">SIMBA es un sistema que permite el manejo de becas alimenticias dentro de la Facultad de Ingenieria.</p>
+        <hr class="my-4">
+        <p>Buscamos siempre mejorar el apoyo a los estudiantes manteniendo en orden el cobro e ingreso de las becas.</p>
+        <!-- <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> --->
     </div>
-    <div class="row">
-        <a class="btn btn-primary" href="modules/alumnos/alumnos.php" role="button">Alumnos</a>
-        <a class="btn btn-primary" href="modules/archivo/archivo.php" role="button">Archivo</a>
-        <a class="btn btn-primary" href="modules/asistencias/asistencias.php" role="button">Asistencias</a>
-        <a class="btn btn-primary" href="modules/becas/becas.php" role="button">Becas</a>
-        <a class="btn btn-primary" href="modules/carreras/carreras.php" role="button">Carreras</a>
-        <a class="btn btn-primary" href="modules/ciclos/ciclos.php" role="button">Ciclos escolares</a>
-        <a class="btn btn-primary" href="modules/estados/estados.php" role="button">Estados</a>
-        <a class="btn btn-primary" href="modules/usuarios/usuarios.php" role="button">Usuarios</a>
-        <a class="btn btn-primary" href="modules/accesos/accesos.php" role="button">Control de accesos</a>
-    </div>
-</div> <!-- /container -->
-</body>
-</html>
+
+    <div class="container">
+        <div class="row col">
+            <a class="btn btn-primary btn-block" href="modules/alumnos/alumnos.php" role="button">Alumnos</a>
+            <a class="btn btn-primary btn-block" href="modules/archivo/archivo.php" role="button">Archivo</a>
+            <a class="btn btn-primary btn-block" href="modules/asistencias/asistencias.php" role="button">Asistencias</a>
+            <a class="btn btn-primary btn-block" href="modules/becas/becas.php" role="button">Becas</a>
+            <a class="btn btn-primary btn-block" href="modules/carreras/carreras.php" role="button">Carreras</a>
+            <a class="btn btn-primary btn-block" href="modules/ciclos/ciclos.php" role="button">Ciclos escolares</a>
+            <a class="btn btn-primary btn-block" href="modules/estados/estados.php" role="button">Estados</a>
+            <a class="btn btn-primary btn-block" href="modules/usuarios/usuarios.php" role="button">Usuarios</a>
+            <a class="btn btn-primary btn-block" href="modules/accesos/accesos.php" role="button">Control de accesos</a>
+        </div>
+    </div> <!-- /container -->
+
+<?php
+include ('views/footer.php');
+?>
